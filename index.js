647bbf56778ce87cf59dbd0ada94f3d9628428e3
Mathieu Houdet <mathieu.houdet@gmail.com>
2019-01-02 02:20:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // write your code below!
 function happyHolidays(){
-  return "Happy holidays!"
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(holiday,name){
+  return `Happy ${holiday}, ${name}`;
 }
